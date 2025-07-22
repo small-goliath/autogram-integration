c4af031f5915860a0f514aa9c8482a9624e32b24
small-goliath <meang617@gmail.com>
2025-07-22 16:58:09 +0900

    chore(gitignore): Google API JSON 파일 무시 규칙 추가
    feat(batch): 카카오톡 파일 업데이트 감지 스크립트 추가
    
    카카오톡 관련 파일의 변경을 실시간으로 감지하고, 필요 시
    의존성을 설치한 후 특정 파이썬 스크립트를 실행하는 기능을
    추가. 이를 통해 카카오톡 파일 업데이트 시 자동으로 처리
    되도록 함.

---
+++ batch/kakaotalk_file_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WATCH_FILE="/home/sch/autogram-integration/batch/kakaotalk/KakaoTalk_latest.txt"
+cd /home/sch/autogram-integration
+source .venv/bin/activate
+
+inotifywait -m -e create,modify "${WATCH_FILE}" | while read FILE
+do
+    pip install -r requirements.txt
+    echo "kakaotalk file update!!!"
+    date
+    python3 -m batch.kakaotalk_active_verify
+done
